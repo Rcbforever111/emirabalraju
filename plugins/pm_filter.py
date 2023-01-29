148145476c70ac43d67ed5ed086c7129f4c878ed
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2023-01-29 16:57:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -416,7 +416,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('😊 About', callback_data='about')
         ],[
             InlineKeyboardButton('💲 Get Premium Access', url='https://telegram.me/tmasupportbot')
-        ]]
+        ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
